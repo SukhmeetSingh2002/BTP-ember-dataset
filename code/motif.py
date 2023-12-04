439da28ba7278988415239d8cce205aa0d0dfde6
Vishnusai <v.sai.j2025@gmail.com>
2023-12-04 11:58:24 +0000

    Add Motif ipynb

---
+++ code/motif.py
@@ -0,0 +1,138 @@
+
+
+import json
+import csv
+import threading
+import math
+import pandas as pd
+import numpy as np
+import multiprocessing
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from xgboost import XGBClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report ,precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split, learning_curve, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.decomposition import PCA
+from scikeras.wrappers import KerasClassifier
+
+import xgboost as xgb
+from scipy.stats import randint
+
+import tensorflow as tf
+
+
+from keras.models import Sequential
+from keras.layers import Input, Dense, Activation, Dropout, Activation
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import load_model
+from keras import regularizers
+from keras import backend as K
+from keras.layers import BatchNormalization
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, GlobalAveragePooling1D, GlobalMaxPooling1D
+from keras.regularizers import l2
+
+import sklearn.model_selection as model_selection
+from sklearn.model_selection import StratifiedKFold
+
+import joblib
+
+import pydot 
+def load_dataset_from_jsonl(file_path):
+    dataset = []
+    with open(file_path, 'r') as file:
+      for line in file:
+          data = json.loads(line)
+          dataset.append(data)
+    return dataset
+
+
+DATA_FILE_PATH = '../../dataset/motif/motif_dataset.jsonl'
+
+
+def extract_features(feature, feature_name, features):
+    
+    for key, value in feature.items(): 
+        
+        string_name = feature_name + '_' + key
+        if string_name not in features:
+            features[string_name]= []
+        
+        if isinstance(value, int) or isinstance(value, float):
+            features[string_name].append(value)
+        else:
+            features[string_name].append(None)
+    
+
+def extract_features_from_dataset_to_dataframe(dataset):
+    
+    features= {}
+    features['label']=[]
+
+    section_names = ['.text', '.data', '.rsrc']
+    feature_names = ['size', 'entropy', 'vsize']
+
+    for section in section_names:
+        for feature in feature_names:
+            features[section +'_' + feature] = []
+
+    features['machine']=[]
+    features['subsystem']=[]
+
+
+    for data in dataset:
+        
+        if data['label'] == -1:
+            continue
+
+        #all sections should be equal to 1    
+        section_count_list = {'.text':0, '.data':0, '.rsrc':0}
+        for item in data['section']['sections']:
+            
+            if item['name'] in section_names:
+                section_count_list[item['name']]+=1
+        
+        check = False
+        for key,value in section_count_list.items():
+            if value!=1:
+                check=True
+                break
+        
+        if check:
+            continue
+            
+        feature_names = ['size', 'entropy', 'vsize']
+            
+        for item in data['section']['sections']:
+
+            if item['name'] in section_names:
+                
+                for feature in feature_names:
+                    val = item[feature] if item[feature]!=None else None
+                    features[item['name'] + '_' + feature].append(val)
+        
+        feature_names = ['general', 'strings']
+
+        for feature in feature_names:
+            extract_features(data[feature], feature,features)
+
+        for item in data['header']:
+            extract_features(data['header'][item], item,features)
+        
+        features['machine'].append(data['header']['coff']['machine'])
+        features['subsystem'].append(data['header']['optional']['subsystem'])
+        
+        
+        features['label'].append(data['label'])
+
+    df = pd.DataFrame(features)
+    print(df['label'].value_counts())
+    return df
+  
+  
